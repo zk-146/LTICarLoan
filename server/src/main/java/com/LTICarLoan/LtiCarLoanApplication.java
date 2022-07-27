61eb69ee56c221c8be662335bb2ee35d0993bcbe
= <=>
2022-07-27 17:04:12 +0530

    Initialize project

---
+++ server/src/main/java/com/LTICarLoan/LtiCarLoanApplication.java
@@ -0,0 +1,12 @@
+package com.LTICarLoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtiCarLoanApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LtiCarLoanApplication.class, args);
+	}
+
+}
