b3f3af3193bcbcf72fa0fb0d16f07e4af1de3bf6
raawhit <rohitdey629@gmail.com>
2022-07-28 11:08:32 +0530

    full loan det

--- server/src/main/java/com/LTICarLoan/beans/LoanDetails.java
+++ server/src/main/java/com/LTICarLoan/beans/LoanDetails.java
@@ -37,6 +37,87 @@ public class LoanDetails {
 		@JoinColumn(name="car_id")
 		VehicleDetails vehicleDetails;
 
+		public LoanDetails(int loan_id, String loan_type, double interest_rate, double loan_amt, int no_of_emi,
+				double processing_fee, VehicleDetails vehicleDetails) {
+			super();
+			this.loan_id = loan_id;
+			this.loan_type = loan_type;
+			this.interest_rate = interest_rate;
+			this.loan_amt = loan_amt;
+			this.no_of_emi = no_of_emi;
+			this.processing_fee = processing_fee;
+			this.vehicleDetails = vehicleDetails;
+		}
+
+		public LoanDetails() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		public int getLoan_id() {
+			return loan_id;
+		}
+
+		public void setLoan_id(int loan_id) {
+			this.loan_id = loan_id;
+		}
+
+		public String getLoan_type() {
+			return loan_type;
+		}
+
+		public void setLoan_type(String loan_type) {
+			this.loan_type = loan_type;
+		}
+
+		public double getInterest_rate() {
+			return interest_rate;
+		}
+
+		public void setInterest_rate(double interest_rate) {
+			this.interest_rate = interest_rate;
+		}
+
+		public double getLoan_amt() {
+			return loan_amt;
+		}
+
+		public void setLoan_amt(double loan_amt) {
+			this.loan_amt = loan_amt;
+		}
+
+		public int getNo_of_emi() {
+			return no_of_emi;
+		}
+
+		public void setNo_of_emi(int no_of_emi) {
+			this.no_of_emi = no_of_emi;
+		}
+
+		public double getProcessing_fee() {
+			return processing_fee;
+		}
+
+		public void setProcessing_fee(double processing_fee) {
+			this.processing_fee = processing_fee;
+		}
+
+		public VehicleDetails getVehicleDetails() {
+			return vehicleDetails;
+		}
+
+		public void setVehicleDetails(VehicleDetails vehicleDetails) {
+			this.vehicleDetails = vehicleDetails;
+		}
+
+		@Override
+		public String toString() {
+			return "LoanDetails [loan_id=" + loan_id + ", loan_type=" + loan_type + ", interest_rate=" + interest_rate
+					+ ", loan_amt=" + loan_amt + ", no_of_emi=" + no_of_emi + ", processing_fee=" + processing_fee
+					+ ", vehicleDetails=" + vehicleDetails + "]";
+		}
+
+		
 		
 
 }
