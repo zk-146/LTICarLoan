0fd3167f773a4bc3ad52b5740a7da6367f4e2492
imrushi45@gmail.com <imrushi45@gmail.com>
2022-07-28 15:50:53 +0530

    VehicleException

--- server/src/main/java/com/LTICarLoan/exception/VehicleException.java
+++ server/src/main/java/com/LTICarLoan/exception/VehicleException.java
@@ -1,5 +1,32 @@
 package com.LTICarLoan.exception;
 
-public class VehicleException {
+public class VehicleException extends Exception{
+
+	public VehicleException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VehicleException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VehicleException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VehicleException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VehicleException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
