647678abf52026bda007512243b4530e19f5fa70
raawhit <rohitdey629@gmail.com>
2022-07-28 15:45:27 +0530

    loanexception

--- server/src/main/java/com/LTICarLoan/exception/LoanException.java
+++ server/src/main/java/com/LTICarLoan/exception/LoanException.java
@@ -1,5 +1,32 @@
 package com.LTICarLoan.exception;
 
-public class LoanException {
+public class LoanException extends Exception{
+
+	public LoanException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoanException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoanException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoanException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoanException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
