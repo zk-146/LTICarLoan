d8935d8dde34a5dbc1c7f95bc095ac8234583273
zk_146 <khanzaid1015@gmail.com>
2022-07-28 10:38:57 +0530

    Add beans- AdminDetails, ApplicationDetails

--- server/src/main/java/com/LTICarLoan/beans/AdminDetails.java
+++ server/src/main/java/com/LTICarLoan/beans/AdminDetails.java
@@ -1,5 +1,24 @@
+
+
 package com.LTICarLoan.beans;
 
-public class AdminDetails {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="admin_details")
+public class AdminDetails {
+	@Id
+	@Column(name="admin_id")
+//	@Seq
+	private int admin_id;
+	
+	@Column(name="username", length=10)
+	private String username;
+	
+	@Column(name="password", length=40)
+	private String password;
 }
+
--- server/src/main/java/com/LTICarLoan/beans/ApplicationDetails.java
+++ server/src/main/java/com/LTICarLoan/beans/ApplicationDetails.java
@@ -1,5 +1,32 @@
 package com.LTICarLoan.beans;
 
-public class ApplicationDetails {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="application_details")
+public class ApplicationDetails {
+	@Id
+	@Column(name="application_id")
+	private int application_id;
+
+	@Column(name="application_status")
+	private String application_status;
+	@Column(name="emi_amt")
+	private int emi_amt;
+	@Column(name="loan_amt")
+	private int loan_amt;	
+	@Column(name="no_of_emis")
+	private int no_of_emis;
+	@Column(name="loan_tenure")
+	private int loan_tenure;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="user_id")
+	UserPersonalDetails userPersonalDetails;
 }
