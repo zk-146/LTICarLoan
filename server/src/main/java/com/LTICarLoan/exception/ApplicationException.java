f060722dd4d706845a2ae3b34a64125dea0e5078
zk_146 <khanzaid1015@gmail.com>
2022-07-28 15:40:55 +0530

    Add Application Exception

--- server/src/main/java/com/LTICarLoan/exception/ApplicationException.java
+++ server/src/main/java/com/LTICarLoan/exception/ApplicationException.java
@@ -1,5 +1,31 @@
 package com.LTICarLoan.exception;
 
-public class ApplicationException {
+public class ApplicationException extends Exception {
+
+	public ApplicationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApplicationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApplicationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApplicationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApplicationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 
 }
