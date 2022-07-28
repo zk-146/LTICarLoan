09b8f68ffdb0ed2cb118243cece66c0765adfc9f
Kamlesh <kamaleshmadan4@gmail.com>
2022-07-28 14:45:51 +0530

    addUser

--- server/src/main/java/com/LTICarLoan/controller/AuthController.java
+++ server/src/main/java/com/LTICarLoan/controller/AuthController.java
@@ -1,5 +1,26 @@
 package com.LTICarLoan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.LTICarLoan.beans.User;
+import com.LTICarLoan.service.UserPersonalService;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/user-api")
 public class AuthController {
+	
+	@Autowired
+	UserPersonalService userService;
+	
+	@PostMapping(path = "/addUser",consumes="application/json")
+	public int addUser(@RequestBody User u) {
+		return userService.addUser(u);
+	}
 
 }
--- server/src/main/java/com/LTICarLoan/dao/UserDao.java
+++ server/src/main/java/com/LTICarLoan/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.LTICarLoan.dao;
 
+import com.LTICarLoan.beans.User;
+
 public interface UserDao {
 
+	public int addUser(User u);
 }
