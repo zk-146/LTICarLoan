f5d8bf06e7ca132e23dc8343a7566f7d6235dcd5
raawhit <rohitdey629@gmail.com>
2022-07-29 19:32:04 +0530

    Register with value done

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -15,31 +15,37 @@ export class RegisterComponent implements OnInit {
           label: "First Name",
           placeholder: "firstName",
           inputType: "text",
+          value: ''
         },
         {
           label: "Last Name",
           placeholder: "lastName",
           inputType: "text",
+          value: ''
         },
         {
           label: "Email ID",
           placeholder: "email",
           inputType: "email",
+          value: ''
         },
         {
           label: "Password",
           placeholder: "password",
           inputType: "text",
+          value: ''
         },
         {
           label: "Age",
           placeholder: "age",
           inputType: "number",
+          value: ''
         },
         {
           label: "Gender",
           placeholder: "Male/Female",
           inputType: "text",
+          value: ''
         },
       ],
     },
@@ -50,26 +56,31 @@ export class RegisterComponent implements OnInit {
           label: "Address",
           placeholder: "address",
           inputType: "text",
+          value: ''
         },
         {
           label: "City",
           placeholder: "Eg. Kolkata",
           inputType: "text",
+          value: ''
         },
         {
           label: "State",
           placeholder: "Eg. West Bengal",
           inputType: "text",
+          value: ''
         },
         {
           label: "Nationality",
           placeholder: "Eg. INDIAN",
           inputType: "text",
+          value: ''
         },
         {
           label: "Pincode",
           placeholder: "Eg. 711227",
           inputType: "number",
+          value: ''
         },
       ],
     },
@@ -80,11 +91,13 @@ export class RegisterComponent implements OnInit {
           label: "Type of Employment",
           placeholder: "Permanent/Temporary",
           inputType: "text",
+          value: ''
         },
         {
           label: "Annual Salary",
           placeholder: "$$$$$",
           inputType: "number",
+          value: ''
         },
       ],
     }
@@ -95,11 +108,16 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   updateFormValue= (titleIndex:any, fieldIndex:any, value:any)=> {
-    this.formDetails[titleIndex].fields[fieldIndex].value.value;
+    this.formDetails[titleIndex].fields[fieldIndex].value=value;
   }
 
   increaseCurrentIndex = () => {
     this.currentFormIndex++;
+  }
+
+  decreaseCurrentIndex = () => {
+    this.currentFormIndex--;
+    console.log(this.formDetails)
   }
 
   ngOnInit(): void {
