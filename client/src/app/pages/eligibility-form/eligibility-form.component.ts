666a67b3cab169ee51d706b651494300a84e0702
zk_146 <khanzaid1015@gmail.com>
2022-07-29 18:20:45 +0530

    Add Eligibility Form structure

--- client/src/app/pages/eligibility-form/eligibility-form.component.ts
+++ client/src/app/pages/eligibility-form/eligibility-form.component.ts
@@ -5,9 +5,98 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './eligibility-form.component.html',
   styleUrls: ['./eligibility-form.component.css']
 })
-export class EligibilityFormComponent implements OnInit {
 
+export class EligibilityFormComponent implements OnInit {
+  formDetails = [
+    {
+      name: "Vehicle Details",
+      fields: [
+        {
+          label: "Car Make",
+          placeholder: "Eg. Nissan",
+          inputType: "text",
+        },
+        {
+          label: "Car Model",
+          placeholder: "Eg. Nissan X39-O2",
+          inputType: "text",
+        },
+        {
+          label: "Ex showroom Price",
+          placeholder: "$77890",
+          inputType: "number",
+        },
+        {
+          label: "On Road Price",
+          placeholder: "$80530",
+          inputType: "number",
+        },
+      ],
+    },
+    {
+      name: "Personal Details",
+      fields: [
+        {
+          label: "First Name",
+          placeholder: "Eg. John",
+          inputType: "text",
+        },
+        {
+          label: "Second Name",
+          placeholder: "Eg. Doe",
+          inputType: "text",
+        },
+        {
+          label: "Age",
+          placeholder: "Eg. 21",
+          inputType: "number",
+        },
+        {
+          label: "Gender",
+          placeholder: "Eg. Male",
+          inputType: "text",
+        },
+        {
+          label: "Type of employment",
+          placeholder: "$80530",
+          inputType: "number",
+        },
+        {
+          label: "Yearly Salary",
+          placeholder: "$77000",
+          inputType: "number",
+        },
+        {
+          label: "Exisitng EMI",
+          placeholder: "Yes",
+          inputType: "text",
+        },
+      ],
+    },
+    {
+      name: "Contact Details",
+      fields: [
+        {
+          label: "Phone Number",
+          placeholder: "Eg. 7718964516",
+          inputType: "number",
+        },
+        {
+          label: "Email",
+          placeholder: "Email",
+          inputType: "email",
+        },
+      ],
+    }
+  ]
+
+  currentFormIndex = 0;
+  
   constructor() { }
+
+  increaseCurrentIndex = () => {
+    this.currentFormIndex++;
+  }
 
   ngOnInit(): void {
   }
