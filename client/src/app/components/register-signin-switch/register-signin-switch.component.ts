150e9bb68cdeadc6aa0e39cc194128021f571de2
raawhit <rohitdey629@gmail.com>
2022-07-29 13:08:23 +0530

    Register

---
+++ client/src/app/components/register-signin-switch/register-signin-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-signin-switch',
+  templateUrl: './register-signin-switch.component.html',
+  styleUrls: ['./register-signin-switch.component.css']
+})
+export class RegisterSigninSwitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
