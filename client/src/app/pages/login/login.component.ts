7377f0451b79e20f2c3876a65cb67be8842ef751
imrushi45@gmail.com <imrushi45@gmail.com>
2022-07-30 11:00:41 +0530

    Validators

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -15,11 +15,16 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
 
+    this.loginForm=this.fb.group({
+      email:['',Validators.required,Validators.email],
+      password:['',Validators.required,Validators.min(4),Validators.max(8)]
+    })
+
   }
 
   public onSubmit(){
    
-    this.loginForm
+   
 
   }
 
