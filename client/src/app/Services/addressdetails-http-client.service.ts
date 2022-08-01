19c90ac485d3d19c716218198108936f17c0bf00
raawhit <rohitdey629@gmail.com>
2022-08-01 11:53:42 +0530

    commit

--- client/src/app/Services/addressdetails-http-client.service.ts
+++ client/src/app/Services/addressdetails-http-client.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class AddressdetailsHttpClientService {
 
-  constructor() { }
+  baseUrl:string="http://localhost:8090/address-api";
+
+  constructor(private httpSer: HttpClient) { }
+
+  public addAddress(address:any){
+    console.log("address IS BEING ADDED", address);
+    return this.httpSer.post<any>(this.baseUrl+'/add-address',address);
+  }
 }
--- client/src/app/Services/employmentdetails-http-client.service.ts
+++ client/src/app/Services/employmentdetails-http-client.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmploymentdetailsHttpClientService {
+  baseUrl:string="http://localhost:8090/employment-api";
 
-  constructor() { }
+  constructor(private httpSer: HttpClient) { }
+  
+  public addemployment(employment:any){
+    console.log("employment IS BEING ADDED", employment);
+    return this.httpSer.post<any>(this.baseUrl+'/add-employment',employment);
+  }
 }
