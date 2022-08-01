7bf008a442bfdb0c0cca717e78c7864b46e6d34f
raawhit <rohitdey629@gmail.com>
2022-08-01 14:54:44 +0530

    Register back integrated

--- server/src/main/java/com/LTICarLoan/beans/User.java
+++ server/src/main/java/com/LTICarLoan/beans/User.java
@@ -2,7 +2,10 @@ package com.LTICarLoan.beans;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -10,6 +13,8 @@ import javax.persistence.Table;
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy= GenerationType.SEQUENCE,generator="USERSEQ")
+	@SequenceGenerator(name="USERSEQ",sequenceName="USERSEQ", allocationSize=1)
 	@Column(name = "user_id")
 	private int user_id;
 
