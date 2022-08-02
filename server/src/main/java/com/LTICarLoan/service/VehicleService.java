4a161e7f8b27104ce6ab19a62e34f21cee10b714
zk_146 <khanzaid1015@gmail.com>
2022-08-02 15:14:29 +0530

    Merge conflicts fixes

--- server/src/main/java/com/LTICarLoan/service/VehicleService.java
+++ server/src/main/java/com/LTICarLoan/service/VehicleService.java
@@ -1,15 +1,17 @@
 package com.LTICarLoan.service;
 
 import com.LTICarLoan.beans.Vehicle;
+import com.LTICarLoan.exception.VehicleException;
 
 public interface VehicleService {
 	
-	public boolean addVehicle(Vehicle v);
+	public boolean addVehicle(Vehicle v) throws VehicleException;
 
-	Vehicle findVehicleByUserId(int user_id);
+	Vehicle findVehicleByUserId(int user_id) throws VehicleException;
 	
 	Vehicle findVehicleById(int id);
 	
+	public boolean deleteVehicleById(int id) throws VehicleException;
 	public boolean deleteVehicleByUserId(int user_id);
 	
 }
