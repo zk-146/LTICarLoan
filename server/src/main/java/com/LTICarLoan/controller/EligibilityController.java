ded1ebb3978bab896507bdefcf395ba4b8c5e821
imrushi45@gmail.com <imrushi45@gmail.com>
2022-08-03 15:08:35 +0530

    refactoring

--- server/src/main/java/com/LTICarLoan/controller/EligibilityController.java
+++ server/src/main/java/com/LTICarLoan/controller/EligibilityController.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.LTICarLoan.beans.Loan;
-import com.LTICarLoan.beans.User;
 import com.LTICarLoan.service.LoanService;
 
 @CrossOrigin(origins = "*")
@@ -19,7 +17,7 @@ import com.LTICarLoan.service.LoanService;
 public class EligibilityController {
 	@Autowired
 	LoanService service;
-	
+
 	@PostMapping(path = "/add-loan/{user_id}", consumes = "application/json")
 	public boolean addLoan(@RequestBody Loan l, @PathVariable("user_id") int user_id) {
 		System.out.println(l);
